a76bd4279e9b9e98409ef8c02ab566aeb918b5f7
catlerc <mcr1123@yandex.ru>
2024-11-04 14:03:28 +0500

    описание предметов в магазе

---
+++ Assets/Scripts/ShopUIItemToolTip.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class ShopUIItemToolTip: MonoBehaviour
+{
+    public ButItemLine line;
+    private void OnMouseOver()
+    {
+        Cursor.instance.updateToolTip(line.item);
+        Cursor.instance.overShopItem = true;
+    }
+}
