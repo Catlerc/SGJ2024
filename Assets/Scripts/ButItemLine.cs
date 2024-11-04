83239e77715a92f57d7fdbba4b459956ee463356
catlerc <mcr1123@yandex.ru>
2024-11-04 17:08:43 +0500

    поправил багу с магазом

--- Assets/Scripts/ButItemLine.cs
+++ Assets/Scripts/ButItemLine.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public class ButItemLine : MonoBehaviour
 {
-    public Item item;
+    public ItemType item;
 
     public TextMeshPro text;
     public SpriteRenderer itemIcon;
     public BuyButton buyButton;
 
-    public void init(Item item)
+    public void init(ItemType item)
     {
         buyButton.item = item;
         this.item = item;
-        text.text = item.type.cost.ToString();
-        itemIcon.sprite = item.type.image;
+        text.text = item.cost.ToString();
+        itemIcon.sprite = item.image;
     }
-}
+}   
--- Assets/Scripts/BuyButton.cs
+++ Assets/Scripts/BuyButton.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public class BuyButton : MonoBehaviour
 {
-    public Item item;
+    public ItemType item;
     public AudioSource paySound;
 
     private void OnMouseUpAsButton()
     {
         var count = Economics.instance.countMoney();
-        if (count >= item.type.cost && Cursor.instance.itemInHand == null)
+        if (count >= item.cost && Cursor.instance.itemInHand == null)
         {
             paySound.Play();
-            Economics.instance.removeMoney(item.type.cost);
-            Cursor.instance.grabItem(item);
+            Economics.instance.removeMoney(item.cost);
+            Cursor.instance.grabItem(new Item(item));
         }
     }
 }
